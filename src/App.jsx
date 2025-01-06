9e8f2634bbdeb9c24dbe16bfe8bea22ddc006d91
Edmundo FuentesRivera <edmundo.fuentesrivera@frappurecords.com>
2025-01-06 17:21:10 -0500

    2024-01-06 4

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,9 @@ import "./estilos.css";
 import "./myStyle.css";
 import "./nav.css";
 
-import { families } from "./data/prodFamilies";
-// console.log(families)
-// const saludar = () => {
-//   alert("Hola click");
-// }
-// const myHr="<hr />"
-// el nombre componente tiene que comenzar con Mayuscula
-const App = () => {
-  // tiene que retornar un solo elemento
-  // console.log("Prueba")
-  // const titulo = "!! Hola React !!"
+// import { families } from "./data/prodFamilies";
 
+const App = () => {
   const [view, setView] = useState('presentation'); // Estado inicial
   const renderView = () => {
     switch (view) {
@@ -50,20 +41,6 @@ const App = () => {
       <div>
         { renderView() }
       </div>
-      {/* <Presentation /> */}
-      <Login />
-
-      <h1>*********</h1>
-      {/* <Families /> */}
-      <h1>*********</h1>
-      <hr />
-
-      {/* <Titulo texto={ titulo } region="Lima" />
-      <Titulo texto="Hello world" region="Arequipa" />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, praesentium? { 10+ 20 }</p>
-      <hr />
-      <button onClick={ saludar } className="texto-verde">Click</button>
-      <Boton texto="CLickea" accion={saludar} /> */}
     </>
   )
 }
