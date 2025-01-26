5fa74104bd5cbac2d5f86e25a03e97d7703f01e7
Edmundo FuentesRivera <edmundo.fuentesrivera@frappurecords.com>
2025-01-26 16:06:50 -0500

    2025-01-26 v7

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Orders = () => {
   return (
-    <div  style={{  position: "fixed", top: "50%",  left: "40%"}}>
-        <h1>Orders ...........</h1>
+
+    <div  style={{ display: "flex", alignItems: "center",  position: "absolute", fontWeight: "600", top: "50%",  left: "50%"}}> 
+
+    <h1 >Orders ... </h1>
+    <i style={{fontSize: "4rem"}} className='fas'>&#xf543;</i>
+
     </div>
   )
 }
