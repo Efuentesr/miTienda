fe04727de32ea3cb8d18332546de2cc46eb56728
Edmundo FuentesRivera <edmundo.fuentesrivera@frappurecords.com>
2025-01-26 16:15:57 -0500

    2025-01-26 v8

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 const Orders = () => {
   return (
-
-    <div  style={{ display: "flex", alignItems: "center",  position: "absolute", fontWeight: "600", top: "50%",  left: "50%"}}> 
-
-    <h1 >Orders ... </h1>
-    <i style={{fontSize: "4rem"}} className='fas'>&#xf543;</i>
+    <div  style={{ display: "flex", alignItems: "center",  position: "absolute",  fontWeight: "600", top: "5rem",  left: "5rem"}}> 
+      <h1 style={{fontSize: "2rem"}}>Orders ... </h1>
+      <i style={{fontSize: "4rem"}} className='fas'>&#xf543;</i>
 
     </div>
   )
